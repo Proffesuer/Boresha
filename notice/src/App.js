574fb7439a9d7efcc7452f1fc22298f5c75f6d53
Proffesuer <levischisira0@gmail.com>
2023-03-17 12:05:21 +0300

    Added new pictures

--- notice/src/App.js
+++ notice/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
 
-<div>
+<div class="container">
+  
 
 <h1>Boresha Online Notice Board system</h1>
 
