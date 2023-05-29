e5ad0735ce54b8d586e5a5f179adbc4fd9067445
Proffesuer <levischisira0@gmail.com>
2023-05-29 12:29:30 +0300

    created a new navigation Bar and deleted my log in page

--- notice_PHP/index.php
+++ notice_PHP/index.php
@@ -1,15 +1,3 @@
-<?php include 'nav.php';?>
 
+<?php include 'navigation.php';?>
 
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Skuli</title>
-</head>
-<body>
-    
-    
-</body>
-</html>
---
+++ notice_PHP/navigation.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+
+<!-- Mirrored from preview.colorlib.com/theme/bootstrap/website-menu-03/ by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 29 May 2023 08:51:30 GMT -->
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link href="../../../../fonts.googleapis.com/css8a7c.css?family=Open+Sans:300,400,700" rel="stylesheet">
+<link rel="stylesheet" href="fonts/icomoon/style.css">
+<link rel="stylesheet" href="css/owl.carousel.min.css">
+
+<link rel="stylesheet" href="css/bootstrap.min.css">
+
+<link rel="stylesheet" href="css/style.css">
+<title>Website Menu #3</title>
+<script nonce="ce8312cb-1415-4448-bef7-3137d2885eea">(function(w,d){!function(a,b,c,d){a[c]=a[c]||{};a[c].executed=[];a.zaraz={deferred:[],listeners:[]};a.zaraz.q=[];a.zaraz._f=function(e){return function(){var f=Array.prototype.slice.call(arguments);a.zaraz.q.push({m:e,a:f})}};for(const g of["track","set","debug"])a.zaraz[g]=a.zaraz._f(g);a.zaraz.init=()=>{var h=b.getElementsByTagName(d)[0],i=b.createElement(d),j=b.getElementsByTagName("title")[0];j&&(a[c].t=b.getElementsByTagName("title")[0].text);a[c].x=Math.random();a[c].w=a.screen.width;a[c].h=a.screen.height;a[c].j=a.innerHeight;a[c].e=a.innerWidth;a[c].l=a.location.href;a[c].r=b.referrer;a[c].k=a.screen.colorDepth;a[c].n=b.characterSet;a[c].o=(new Date).getTimezoneOffset();if(a.dataLayer)for(const n of Object.entries(Object.entries(dataLayer).reduce(((o,p)=>({...o[1],...p[1]})),{})))zaraz.set(n[0],n[1],{scope:"page"});a[c].q=[];for(;a.zaraz.q.length;){const q=a.zaraz.q.shift();a[c].q.push(q)}i.defer=!0;for(const r of[localStorage,sessionStorage])Object.keys(r||{}).filter((t=>t.startsWith("_zaraz_"))).forEach((s=>{try{a[c]["z_"+s.slice(7)]=JSON.parse(r.getItem(s))}catch{a[c]["z_"+s.slice(7)]=r.getItem(s)}}));i.referrerPolicy="origin";i.src="../../../cdn-cgi/zaraz/sd0d9.js?z="+btoa(encodeURIComponent(JSON.stringify(a[c])));h.parentNode.insertBefore(i,h)};["complete","interactive"].includes(b.readyState)?zaraz.init():a.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,"zarazData","script");})(window,document);</script></head>
+<body>
+<div class="site-mobile-menu">
+<div class="site-mobile-menu-header">
+<div class="site-mobile-menu-close mt-3">
+<span class="icon-close2 js-menu-toggle"></span>
+</div>
+</div>
+<div class="site-mobile-menu-body"></div>
+</div>
+<header class="site-navbar" role="banner">
+<div class="container">
+<div class="row align-items-center">
+<div class="col-11 col-xl-2">
+<h1 class="mb-0 site-logo"><a href="index-2.html" class="text-white mb-0">Brand</a></h1>
+</div>
+<div class="col-12 col-md-10 d-none d-xl-block">
+<nav class="site-navigation position-relative text-right" role="navigation">
+<ul class="site-menu js-clone-nav mr-auto d-none d-lg-block">
+<li class="active"><a href="index-2.html"><span>Home</span></a></li>
+<li class="has-children">
+<a href="about.html"><span>Dropdown</span></a>
+<ul class="dropdown arrow-top">
+<li><a href="#">Menu One</a></li>
+<li><a href="#">Menu Two</a></li>
+<li><a href="#">Menu Three</a></li>
+<li class="has-children">
+<a href="#">Dropdown</a>
+<ul class="dropdown">
+<li><a href="#">Menu One</a></li>
+<li><a href="#">Menu Two</a></li>
+<li><a href="#">Menu Three</a></li>
+<li><a href="#">Menu Four</a></li>
+</ul>
+</li>
+</ul>
+</li>
+<li><a href="listings.html"><span>Listings</span></a></li>
+<li><a href="about.html"><span>About</span></a></li>
+<li><a href="blog.html"><span>Blog</span></a></li>
+<li><a href="contact.html"><span>Contact</span></a></li>
+</ul>
+</nav>
+</div>
+<div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
+</div>
+</div>
+</div>
+</header>
+<div class="hero" style="background-image: url('images/hero_1.jpg');"></div>
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.sticky.js"></script>
+<script src="js/main.js"></script>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v52afc6f149f6479b8c77fa569edb01181681764108816" integrity="sha512-jGCTpDpBAYDGNYR5ztKt4BQPGef1P0giN6ZGVUi835kFF88FOmmn8jBQWNgrNd8g/Yu421NdgWhwQoaOPFflDw==" data-cf-beacon='{"rayId":"7ced7eece9a64f6a","version":"2023.4.0","b":1,"token":"cd0b4b3a733644fc843ef0b185f98241","si":100}' crossorigin="anonymous"></script>
+</body>
+
+<!-- Mirrored from preview.colorlib.com/theme/bootstrap/website-menu-03/ by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 29 May 2023 08:51:48 GMT -->
+</html>
