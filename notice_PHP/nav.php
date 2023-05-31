17ef7b02f00c55ce7f8a8a0026e841c65c85bac5
Proffesuer <levischisira0@gmail.com>
2023-05-31 14:01:17 +0300

    moved cards inside navigation bar

--- notice_PHP/nav.php
+++ notice_PHP/nav.php
@@ -28,7 +28,7 @@
 <div class="container">
 <div class="row align-items-center">
 <div class="col-6 col-xl-2">
-<h1 class="mb-0 site-logo "><a href="index.html"><b>VARSITY</b><span class="text-primary"></span> </a></h1>
+<h1 class="mb-0 site-logo "><a href="./"><b>VARSITY</b><span class="text-primary"></span> </a></h1>
 </div>
 <div class="col-12 col-md-10 d-none d-xl-block">
 <nav class="site-navigation position-relative text-right" role="navigation"style="background:#053742;border-radius:20px;">
@@ -65,7 +65,15 @@
 </div>
 </div>
 </header>
-<div class="hero"></div>
+<div class="hero"> 
+<br><br><br>
+<div class="container">
+
+
+<?php include 'Home.php';?>
+
+</div>
+</div>
 <script src="js/jquery-3.3.1.min.js"></script>
 <script src="js/popper.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
