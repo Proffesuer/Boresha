df3966d5eb2a37cff6e6f6a1ec9ef7b43b5c7bf4
Proffesuer <levischisira0@gmail.com>
2023-06-07 12:36:01 +0300

    Created a connection to database

--- notice_PHP/Database/config.php
+++ notice_PHP/Database/config.php
@@ -1,14 +1,20 @@
 <?php
 
 
+$servername ="localhost";
+$username ="root";
+$pass ="";
 
+//connection to the database 
+$conn = new mysqli($servername ,$username, $pass);
 
+//check connection
+if($conn->connect_error){
 
+die ("Connection to Notice Database Failled!!" .$conn->connect_error);
 
-
-
-
-
+}
+echo "Connection Is successfull";
 
 
 ?>
