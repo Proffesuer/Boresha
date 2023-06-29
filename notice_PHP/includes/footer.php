06699dfae3c8ff76eb56526bf302223cfa831939
Proffesuer <levischisira0@gmail.com>
2023-06-29 05:17:16 +0300

    seperated the navigation bar

---
+++ notice_PHP/includes/footer.php
@@ -0,0 +1,12 @@
+ 
+ <!-- Footer -->
+ <footer class="sticky-footer bg-primary"style="Color:#fff;">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Bennavi Softsolution 2023</span>
+                    </div>
+                </div>
+            </footer>
+
+            
+            <!--End of footer-->
