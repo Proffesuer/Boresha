f0f8a952c1a0f2179bb3e714137578014f6e40a7
Proffesuer <levischisira0@gmail.com>
2023-07-01 12:52:53 +0300

    created title

--- notice_PHP/teacher/timetable.php
+++ notice_PHP/teacher/timetable.php
@@ -15,7 +15,7 @@ session_start();
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Boresha Smart Notice</title>
+    <title>Timetable</title>
 
     <!-- Custom fonts for this template-->
     <link href="../Dash_style/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
